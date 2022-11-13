2ba70ae7db56a66375979ea6f5628c13a9ee5680
Deepak Saini <59208626+saini1397@users.noreply.github.com>
2022-11-13 14:26:55 +0530

    Update data.R

--- data-raw/data.R
+++ data-raw/data.R
@@ -1,7 +1,5 @@
 rm(list = ls())
 
-
-
 file=list.files(path="data/",pattern = ".xpt")
 
 len <- length(file)
@@ -13,3 +11,18 @@ for (i in 1:len){
 # Keep all ADaM into one vector.
 
 adam_list=ls(pattern="ad")
+
+# Get Datasets for Figure which contains PARAM/AVAL variable.
+adam_listf=c()
+for (i in 1:length(adam_list)){
+  if ( (length( names(get(adam_list[i]))[names(get(adam_list[i])) %in% c("PARAM")]) !=0)
+       )
+    adam_listf[i]=adam_list[i]
+}
+
+adam_listf[!is.na(adam_listf)]
+
+
+tot <- adsl  %>% group_by(TRT01A) %>% 
+             summarise(tot=n_distinct(USUBJID)) 
+
